39b4312e25dfd3ca41185b8ec7b00ecb95915b25
Alex van Vliet <alex.van-vliet@epita.fr>
2023-04-05 13:35:11 +0200

    feat: find language and compute link

--- plugin/linkify/plugin.js
+++ plugin/linkify/plugin.js
@@ -6,13 +6,29 @@ const defaultConfig = {
   languages,
 };
 
+const addLink = (config, block) => {
+  const language = Array.from(block.classList).find(className => config.languages.hasOwnProperty(className));
+
+  if (language === undefined) {
+    return;
+  }
+
+  const code = block.textContent;
+
+  const link = config.languages[language].link(code);
+
+  console.log(link);
+};
+
 const Plugin = () => {
   return {
     id: 'linkify',
     init(deck) {
       const config = merge({}, defaultConfig, deck.getConfig().linkify || {});
 
-      console.log(config);
+      const blocks = Array.from(deck.getRevealElement().getElementsByTagName('code'));
+
+      blocks.forEach(block => addLink(config, block));
     },
   };
 };
